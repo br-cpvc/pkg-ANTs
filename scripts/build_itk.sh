4c0a1a9ed93c1b910f5e4b9f61a00a64ab736889
Christian P. V. Christoffersen <88783438+br-cpvc@users.noreply.github.com>
2022-12-05 14:35:53 +0000

    downgraded to build 2.3.0 using ubuntu 12.04 and gcc 5 and itk 5.0b01, using new git to download itk modules

--- scripts/build_itk.sh
+++ scripts/build_itk.sh
@@ -15,16 +15,16 @@ if [ -d $itk_dir ]; then
     cmake .. \
 	-DCMAKE_INSTALL_PREFIX=../install \
 	-DCMAKE_BUILD_TYPE=Release \
-	-DCMAKE_C_COMPILER=/usr/bin/gcc-11 \
-	-DCMAKE_CXX_COMPILER=/usr/bin/g++-11 \
+	-DCMAKE_C_COMPILER=/usr/bin/gcc-5 \
+	-DCMAKE_CXX_COMPILER=/usr/bin/g++-5 \
 	-DBUILD_EXAMPLES=OFF \
 	-DBUILD_TESTING=OFF \
 	-DITK_LEGACY_REMOVE:BOOL=OFF \
-	-DBUILD_SHARED_LIBS=OFF \
-	-DITK_DYNAMIC_LOADING=OFF \
 	-DModule_GenericLabelInterpolator:BOOL=ON \
 	-DModule_AdaptiveDenoising:BOOL=ON \
 	-DModule_ITKReview=ON
+	#-DBUILD_SHARED_LIBS=OFF \
+	#-DITK_DYNAMIC_LOADING=OFF \
     n=`nproc --ignore 1`
     make -j $n install
     cd $cwd
