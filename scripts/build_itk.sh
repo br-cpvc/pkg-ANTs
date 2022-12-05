0bc35a9f46f7fc0170e4d95638289298d3376ead
Christian P. V. Christoffersen <88783438+br-cpvc@users.noreply.github.com>
2022-12-05 14:35:53 +0000

    downgraded to build 2.3.1 using ubuntu 12.04 and gcc 4.8

--- scripts/build_itk.sh
+++ scripts/build_itk.sh
@@ -15,15 +15,13 @@ if [ -d $itk_dir ]; then
     cmake .. \
 	-DCMAKE_INSTALL_PREFIX=../install \
 	-DCMAKE_BUILD_TYPE=Release \
-	-DCMAKE_C_COMPILER=/usr/bin/gcc-11 \
-	-DCMAKE_CXX_COMPILER=/usr/bin/g++-11 \
+	-DCMAKE_C_COMPILER=/usr/bin/gcc-4.8 \
+	-DCMAKE_CXX_COMPILER=/usr/bin/g++-4.8 \
 	-DBUILD_EXAMPLES=OFF \
 	-DBUILD_TESTING=OFF \
 	-DITK_LEGACY_REMOVE:BOOL=OFF \
 	-DBUILD_SHARED_LIBS=OFF \
 	-DITK_DYNAMIC_LOADING=OFF \
-	-DModule_GenericLabelInterpolator:BOOL=ON \
-	-DModule_AdaptiveDenoising:BOOL=ON \
 	-DModule_ITKReview=ON
     n=`nproc --ignore 1`
     make -j $n install
